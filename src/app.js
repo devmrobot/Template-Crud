5bc95f6eb003c18c1985b6ec3217554202fe2611
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2022-05-30 15:33:38 +0200

    workshop done

--- src/app.js
+++ src/app.js
@@ -1,27 +1,44 @@
-require('dotenv').config();
-const connection = require('./db-config');
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const connection = require("./db-config");
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const router = require('./routes/index.routes');
+const router = require("./routes/index.routes");
 
 const port = process.env.PORT || 8000;
 
 connection.connect((err) => {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
   } else {
-    console.log('connected as id ' + connection.threadId);
+    console.log("connected as id " + connection.threadId);
   }
 });
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cors())
-app.use('/api', router);
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/api", router);
+
+
+const extractUserName = (req, res, next) => {
+  if (req.query.name) {
+    req.userName = req.query.name;
+    next();
+  } else {
+    res.status(400)
+       .send('You have to specify a "name" query parameter to call this route');
+  }
+};
+
+app.get('/hello', extractUserName, (req, res) => {
+ console.log('handling /hello');
+ res.send(`Hello ${req.userName} !`);
+});
+
 
 app.get("/", (req, res) => {
-    res.send("Welcome");
+  res.send("Welcome");
 });
 
 app.listen(port, () => {
